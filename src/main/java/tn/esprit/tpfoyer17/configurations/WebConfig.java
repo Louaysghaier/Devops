e81e49c68ca71914fbb9908349a5df89bd230414
sghaier louay <115406038+Louaysghaier@users.noreply.github.com>
2024-11-14 20:39:47 +0100

    config angular

---
+++ src/main/java/tn/esprit/tpfoyer17/configurations/WebConfig.java
@@ -0,0 +1,16 @@
+package tn.esprit.tpfoyer17.configurations;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow CORS on all endpoints
+                .allowedOrigins("http://192.168.1.17/:4200")  // Allow Angular frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow specific HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow credentials (e.g., cookies or HTTP authentication)
+    }
+}
